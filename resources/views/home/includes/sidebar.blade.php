2c4e780f2706ed402b817c35b594db97e8803b6a
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-04 14:56:41 +0700

    1

--- resources/views/home/includes/sidebar.blade.php
+++ resources/views/home/includes/sidebar.blade.php
@@ -3,19 +3,24 @@
 
         <ul class="sidebar-nav" id="sidebar-nav">
 
+            {{-- home --}}
             <li class="nav-item">
                 <a class="nav-link " href="{{ route('home') }}">
                     <i class="bi bi-grid"></i>
                     <span>Home</span>
                 </a>
             </li>
-            
-            <li class="nav-item">
-                <a class="nav-link " href="{{ route('category.index') }}">
-                    <i class="bi bi-basket2"></i>
-                    <span>category</span>
-                </a>
-            </li>
+
+            @if (Auth::user()->role == 'admin')
+                {{-- cstegory & news --}}
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('category.index') }}">
+                        <i class="bi bi-basket2"></i>
+                        <span>category</span>
+                    </a>
+                </li>
+            @else
+            @endif
         </ul>
 
     </aside><!-- End Sidebar-->
