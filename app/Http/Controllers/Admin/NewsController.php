e487708b0767d3606ab89223c1ea777461834526
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-05 15:00:24 +0700

    1

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -43,6 +43,12 @@ class NewsController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request,[
+            'title' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:5120',
+            'content' =>'required',
+            'category_id' => 'required',
+        ]);
     }
 
     /**
