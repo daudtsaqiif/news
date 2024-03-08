ec3f59c82acf9f2ef7d0884d1a2379d11070306e
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-08 09:08:35 +0700

    1

--- resources/views/home/includes/sidebar.blade.php
+++ resources/views/home/includes/sidebar.blade.php
@@ -14,16 +14,23 @@
             @if (Auth::user()->role == 'admin')
                 {{-- cstegory & news --}}
                 <li class="nav-item">
-                    <a class="nav-link " href="{{ route('category.index') }}">
-                        <i class="bi bi-basket2"></i>
-                        <span>category</span>
+                    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
+                        href="#">
+                        <i class="bi bi-menu-button-wide"></i><span>Components</span><i
+                            class="bi bi-chevron-down ms-auto"></i>
                     </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="{{ route('news.index') }}">
-                        <i class="bi bi-basket2"></i>
-                        <span>News</span>
-                    </a>
+                    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                        <li>
+                            <a href="{{ route('category.index') }}">
+                                <i class="bi bi-circle"></i><span>Category</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('news.index') }}">
+                                <i class="bi bi-circle"></i><span>News</span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
                 
             @else
