96089173d6f16a8ce14f0846cef75c402bc4f6d9
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-13 11:17:57 +0700

    1

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -17,5 +18,38 @@ class ProfileController extends Controller
         $title = 'Change - Password';
 
         return view('home.profile.change-password', compact('title'));
+    }
+
+    public function updatePassword(Request $request){
+        //validate
+        $request->validate($request,[
+            'current_password' => 'required',
+            'password' => 'required',
+            'confirmation_password' => 'required'
+        ]);
+
+        //check current password status
+        $currentPasswordStatus = Hash::check(
+            $request->current_password, auth()->user()->password,
+        );
+
+        if($currentPasswordStatus){
+            if($request->password == $request->confirmation_password){
+                //update password
+                //get user login by id
+                $user = auth()->user();
+
+                //update password
+                $user->password = Hash::make($request->password);
+                $user->save();
+
+                return redirect()->back()->with('success', 'password has ben update');
+            } else{
+                return redirect()->back()->with('error', 'password does not mach');
+            }
+        }else{
+            return redirect()->back()->with('error', 'Current password is worng');
+        }
+
     }
 }
--- resources/views/home/profile/change-password.blade.php
+++ resources/views/home/profile/change-password.blade.php
@@ -2,28 +2,46 @@
 
 
 @section('content')
+
     <div class="row">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                <strong>{{ session('success') }}</strong>
+            </div>
+        @endif
+
         <div class="card p-4">
             <h3 class="card-title">Change Password</h3>
             <form action="" method="post">
                 @csrf
                 @method('PUT')
                 <div class="row mb-3">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Current  Password</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Current Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="Current Password">
+                        <input name="current_password" type="password" class="form-control" placeholder="Current Password">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="New Password">
+                        <input name="password" type="password" class="form-control" placeholder="New Password">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Confirmation Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="Confirmation Password">
+                        <input name="confrimation_password" type="password" class="form-control"
+                            placeholder="Confirmation Password">
                     </div>
                 </div>
 
