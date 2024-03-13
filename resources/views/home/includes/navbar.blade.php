2e1bfc73ae7dc165a36266765c4d18f2e1553852
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-13 10:11:26 +0700

    1

--- resources/views/home/includes/navbar.blade.php
+++ resources/views/home/includes/navbar.blade.php
@@ -170,7 +170,7 @@
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                     data-bs-toggle="dropdown">
-                    <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" alt="Profile" class="rounded-circle">
+                    <img src="https://ui-avatars.com/api/?background=random&name={{ Auth::user()->name }}" alt="Profile" class="rounded-circle">
                     <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
                 </a><!-- End Profile Iamge Icon -->
 
@@ -184,7 +184,7 @@
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                        <a class="dropdown-item d-flex align-items-center" href="{{ route('profile.index') }}">
                             <i class="bi bi-person"></i>
                             <span>My Profile</span>
                         </a>
@@ -196,19 +196,13 @@
                     <li>
                         <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                             <i class="bi bi-gear"></i>
-                            <span>Account Settings</span>
+                            <span>Change Password</span>
                         </a>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
 
-                    <li>
-                        <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                            <i class="bi bi-question-circle"></i>
-                            <span>Need Help?</span>
-                        </a>
-                    </li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
--- resources/views/home/profile/index.blade.php
+++ resources/views/home/profile/index.blade.php
@@ -1,5 +1,21 @@
 @extends('home.parent')
 
 @section('content')
-ini halaman profile {{ Auth::user()->name }}
+<div class="card p-4 ">
+    <div class="row">
+        <div class="col-md-6 d-flex justify-content-center">
+            <img class="w-75" src="https://ui-avatars.com/api/?background=random&name={{ Auth::user()->name }}" alt="ini nama user">
+        </div>
+        
+        <div class="col-md-6 text-center">
+            <h3>Profile Account</h3>
+            <ul class="list-group">
+                <li class="list-group-item" aria-current="true">Name Account =  <strong>{{ Auth::user()->name }}</strong> </li>
+                <li class="list-group-item">E-Mail Account =<strong> {{ Auth::user()->email }}</strong></li>
+                <li class="list-group-item">Role Account = <strong> {{ Auth::user()->role }}</strong></li>
+            </ul>
+        </div>
+    </div>
+    
+</div>
 @endsection
