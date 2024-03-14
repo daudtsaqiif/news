fec73d2e1a52dbccbb6c5af3c2522893505f27dc
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-14 10:44:50 +0700

    1

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -64,7 +64,9 @@
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary"
                                                     data-bs-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                <button type="button" class="btn btn-success">
+                                                    <i class="bi bi-pencil"></i>
+                                                    Reset Password</button>
                                             </div>
                                         </div>
                                     </div>
