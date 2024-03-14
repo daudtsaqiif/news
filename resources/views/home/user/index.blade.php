9b201aad582ec1e3090076321d45938a83172f1a
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-14 10:43:44 +0700

    1

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
     <div class="row">
+        
+        @if (session('error'))
+            <div class="alert alert-danger">
+                <strong>{{ session('error') }}</strong>
+            </div>
+        @endif
+
+        @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                <strong>{{ session('success') }}</strong>
+            </div>
+        @endif
         <div class="card p-4">
             <h3 class="card-title">
                 All User
@@ -23,23 +45,21 @@
                             <td>{{ $row->name }}</td>
                             <td>{{ $row->email }}</td>
                             <td>{{ $row->role }}</td>
-                            <td> <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#basicModal">
-                                    Basic Modal
+                            <td> <button type="button" class="btn btn-warning" data-bs-toggle="modal"
+                                    data-bs-target="#basicModal{{ $row->id }}">
+                                    <i class="bi bi-pencil"></i>
+                                    Reset Password
                                 </button>
-                                <div class="modal fade" id="basicModal" tabindex="-1">
+                                <div class="modal fade" id="basicModal{{ $row->id }}" tabindex="-1">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title">Basic Modal</h5>
+                                                <h5 class="modal-title">Reset Password <strong>{{ $row->name}}</strong> </h5>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                Non omnis incidunt qui sed occaecati magni asperiores est mollitia. Soluta
-                                                at et reprehenderit. Placeat autem numquam et fuga numquam. Tempora in
-                                                facere consequatur sit dolor ipsum. Consequatur nemo amet incidunt est
-                                                facilis. Dolorem neque recusandae quo sit molestias sint dignissimos.
+                                                Deafault Password become to <strong>123456</strong>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary"
@@ -48,7 +68,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div><!-- End Basic Modal-->
+                                </div>
                             </td>
                         </tr>
                     @endforeach
