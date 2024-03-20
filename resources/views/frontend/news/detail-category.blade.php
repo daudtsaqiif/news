0b9e9ae470a06a0a865938ba3696056fbe867360
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-20 13:06:54 +0700

    1

--- resources/views/frontend/news/detail-category.blade.php
+++ resources/views/frontend/news/detail-category.blade.php
@@ -6,14 +6,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-9" data-aos="fade-up">
-                    <h3 class="category-title">Category: Business</h3>
+                    <h3 class="category-title">Category:</h3>
 
                     <div class="d-md-flex post-entry-2 half">
                         <a href="single-post.html" class="me-4 thumbnail">
                             <img src="assets/img/post-landscape-6.jpg" alt="" class="img-fluid">
                         </a>
                         <div>
-                            <div class="post-meta"><span class="date">Culture</span> <span class="mx-1">&bullet;</span>
+                            <div class="post-meta"><span class="date"></span> <span class="mx-1">&bullet;</span>
                                 <span>Jul 5th '22</span></div>
                             <h3><a href="single-post.html">What is the son of Football Coach John Gruden, Deuce Gruden doing
                                     Now?</a></h3>
@@ -29,6 +29,76 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                
+                <div class="col-md-3">
+                    <!-- ======= Sidebar ======= -->
+                    <div class="aside-block">
+
+                        <ul class="nav nav-pills custom-tab-nav mb-4" id="pills-tab" role="tablist">
+                            @foreach ($category as $row => $index)
+                                <li class="nav-item" role="presentation">
+                                    <button class="nav-link {{ $row == 0 ? 'active' : '' }}" id="pills-popular-tab"
+                                        data-bs-toggle="pill" data-bs-target="#pills-popular{{ $index->id }}"
+                                        type="button" role="tab" aria-controls="pills-popular{{ $index->id }}"
+                                        aria-selected="true">{{ $index->name }}</button>
+                                </li>
+                            @endforeach
+                        </ul>
+
+                        <div class="tab-content" id="pills-tabContent">
+
+                            @foreach ($category as $row => $index)
+                                <div class="tab-pane fade show {{ $row == 0 ? 'active' : '' }}"
+                                    id="pills-popular{{ $index->id }}" role="tabpanel"
+                                    aria-labelledby="pills-popular-tab">
+                                    @foreach ($index->news->sortByDesc('created_at')->take(3) as $news)
+                                        <div class="post-entry-1 border-bottom">
+                                            <div class="post-meta"><span class="date">{{ $index->name }}</span> <span
+                                                    class="mx-1">&bullet;</span>
+                                                <span>{{ $news->created_at->diffForHumans() }}</span></div>
+                                            <h2 class="mb-2"><a
+                                                    href="{{ route('detailNews', $news->slug) }}">{{ $news->title }}</a>
+                                            </h2>
+                                            <span class="author mb-3 d-block">Admin</span>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+
+                    <div class="aside-block">
+                        <h3 class="aside-title">Video</h3>
+                        <div class="video-post">
+                            <a href="https://www.youtube.com/watch?v=AiFfDjmd0jU" class="glightbox link-video">
+                                <span class="bi-play-fill"></span>
+                                <img src="assets/img/post-landscape-5.jpg" alt="" class="img-fluid">
+                            </a>
+                        </div>
+                    </div>
+                    <!-- End Video -->
+
+                    <div class="aside-block">
+                        <h3 class="aside-title">Categories</h3>
+                        <ul class="aside-links list-unstyled">
+                            @foreach ($category as $row)
+                                <li><a href="{{ route('detailCategory', $row->slug) }}"><i class="bi bi-chevron-right"></i>
+                                        {{ $row->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div><!-- End Categories -->
+
+                    <div class="aside-block">
+                        <h3 class="aside-title">Tags</h3>
+                        <ul class="aside-tags list-unstyled">
+                            @foreach ($category as $row)
+                                <li><a href="{{ route('detailCategory', $row->slug) }}">{{ $row->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div><!-- End Tags -->
+
                 </div>
             </div>
     </section>
