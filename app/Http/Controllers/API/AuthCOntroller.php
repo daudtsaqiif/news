958833bdff103f81e90217666f59555c984406e9
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-03-21 10:13:45 +0700

    1

--- app/Http/Controllers/API/AuthCOntroller.php
+++ app/Http/Controllers/API/AuthCOntroller.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthCOntroller extends Controller
 {
@@ -19,6 +21,12 @@ class AuthCOntroller extends Controller
 
             //cek credentials (login)
             $credentials = request(['email', 'password']);
+            if (!Auth::attempt([
+                'email' => $credentials['email'],
+                'password' => $credentials['password']
+            ])){
+                return ResponseFormatter::error([]);
+            };
         }catch (Exception $error){
 
         }
